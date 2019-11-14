5c574d0eac277f4ca175783547434038dab7e51e
armanfar <ramin.armanfar@msg.group>
2019-11-14 12:31:21 +0100

    init

---
+++ www/site.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>My PHP page</title>
+    </head>
+    <body>
+        <h1>Welcome</h1>
+        <?php
+            echo("Hello World");
+        ?>
+    </body>
+</html>
